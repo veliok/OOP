f402f7cb2066421cfcdd6969654986bcb4d5449a
veliok <veli.okkonen@gmail.com>
2025-01-21 20:25:03 +0200

    Initial commit

---
+++ Week3/main.cpp
@@ -0,0 +1,16 @@
+#include "ItalianChef.h"
+
+using namespace std;
+
+int main()
+{
+    Chef chef("Gordon");
+    chef.makeSalad(11);
+    chef.makeSoup(14);
+
+    ItalianChef chef2("Mario");
+    chef2.makeSalad(9);
+    chef2.askSecret("pizza", 12, 12);
+
+    return 0;
+}
